a3e756992d9af17a2bb7b6ecaa26d4e9f8c29ee8
james54068 <ja19891203mes@hotmail.com>
2015-01-06 00:13:55 +0800

    real time write works at max speed 100KBytes

--- can/main.c
+++ can/main.c
@@ -144,6 +144,13 @@ void GPIO_Configuration(void)
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
 
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOG, &GPIO_InitStructure);
+
     // GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
     // GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
     // GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
